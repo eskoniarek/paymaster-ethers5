14a1407d977a5b9390b8d82c3c030796089f158e
eskoniarek <skoniarek93@gmail.com>
2024-06-17 23:02:19 +0200

    [feat] logic and next 14 fe

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Paymaster from './../modules/components/paymaster';
+     
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <Paymaster />
+    </div>
+  );
+}
